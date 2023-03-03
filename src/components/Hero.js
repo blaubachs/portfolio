24f92d19560f1d60b39bfc58c68bebca02e8124b
Ben L <veloswim@gmail.com>
2023-03-02 16:52:49 -0800

    added some description to hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Hero() {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <section className="left-side">
+        <h2>ABOUT ME</h2>
+      </section>
+      <section className="right-side">
+        <p>
+          My name is Ben Laubach. Currently, I am studying full stack web
+          development in the University of Washington's coding bootcamp. Feel
+          free to take a look around my github for my work, or contact me.
+        </p>
+      </section>
+    </div>
+  );
 }
