18200c74e5f59b7cd2ccb15fe3bad166094cb475
Ben L <veloswim@gmail.com>
2023-03-02 19:15:14 -0800

    added left and right side, work card component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,13 @@
 import React from "react";
+import WorkCard from "./WorkCard";
 
 export default function Work() {
-  return <div className="work">Work</div>;
+  return (
+    <div className="work">
+      <section className="left-side">MY WORK</section>
+      <section className="right-side">
+        <WorkCard />
+      </section>
+    </div>
+  );
 }
---
+++ src/components/WorkCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WorkCard(props) {
+  return <div>WorkCard</div>;
+}
