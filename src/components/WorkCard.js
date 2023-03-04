1b0399617b28ef1ffda98d03c2a5588c9e1aa3a1
Ben L <veloswim@gmail.com>
2023-03-03 21:53:08 -0800

    adjusted css for work card

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "../assets/css/WorkCard.css";
 
-import BGP from "../assets/img/Board-Game-Party.png";
-import WTA from "../assets/img/wta-home.jpg";
-import TWO from "../assets/img/twogg.PNG";
-
 export default function WorkCard(props) {
   return (
     <div className="work-card">
